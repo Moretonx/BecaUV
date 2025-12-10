a01f6a59cd431cfe6da3d04d1450dc6a3598183c
Moretonx <franciscomoretti327@gmail.com>
2025-12-10 04:17:30 -0300

    plantilla UV corregida

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   /* backend: 'http://backend_becasuv:4000'
-  backend: 'http://api.becas-informatica.uv.cl' */
+  backend: 'http://api.becas-informatica.uv.cl'*/
   backend: 'https://becauv-production-393b.up.railway.app'
 };
 
